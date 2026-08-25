d3a17a05ca28d364e962862e0e81d57133e3268f
Cristopher Anampa <314065901+crisanampa@users.noreply.github.com>
2026-08-24 21:56:43 -0500

    Calcular resto de 2 numeros.

---
+++ calcular-resto.py
@@ -0,0 +1,8 @@
+# Se estan calculando el resto de dos numeros
+
+num_1 = int(input('Ingrese un numero: '))
+num_2 = int(input('Ingrese el segundo numero: '))
+
+rest = num_1 % num_2
+
+print('El resto de la division de ambos numeros es: ', rest)
